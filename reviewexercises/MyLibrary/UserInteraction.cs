19817b69c6614ade0b56328d4d6e56f12502e8aa
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-18 19:25:45 +0100

    Finish exercise 2, closes #3

---
+++ reviewexercises/MyLibrary/UserInteraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class UserInteraction
+    {
+        public static float UserInputFloat()
+        {
+            float userNum;
+            try
+            {
+                userNum = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                userNum = 0.0f;
+            }
+            return userNum;
+        }
+    }
+}
