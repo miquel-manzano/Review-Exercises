387b80b7c8a85df66a173e5c11171dd9a3d69fac
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-20 22:57:53 +0100

    Finish exercise 9.

---
+++ reviewexercises/MyLibrary/MyMath.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class MyMath
+    {
+        public static float CalculateTipPercentage(int priceNum)
+        {
+            return priceNum * 0.20f;
+        }
+    }
+}
