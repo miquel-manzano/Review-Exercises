6ca0e6eb0cfb5cceba6350bc9818cc61623752d0
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-20 22:04:34 +0100

    Finish exercise 6.

---
+++ reviewexercises/MyLibrary/UserInteraction.cs
@@ -0,0 +1,19 @@
+﻿namespace MyLibrary
+{
+    public class UserInteraction
+    {
+        public static float UserInputFloat()
+        {
+            float userNum;
+            try
+            {
+                userNum = float.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                userNum = 0.0f;
+            }
+            return userNum;
+        }
+    }
+}
